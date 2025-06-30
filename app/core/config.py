0cfb37ebe39cb94fd2b660c259b75c325f724fbf
Neha-Jacob <60283339+Neha-Jacob@users.noreply.github.com>
2025-06-30 23:51:05 +0200

    feat: include docker setup

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,24 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, PostgresDsn
+from typing import Optional
+
 
 class Settings(BaseSettings):
     app_name: str = "Auth and User Management Service"
-    database_url: str
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str = "userdb"
+    postgres_port: str = "5432"
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
